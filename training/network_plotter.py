a7c6788890b757df90073728d12e15a340e22a85
Jonathan Ho <jonathanho@Jonathans-MacBook-Pro-3.local>
2020-01-28 08:51:51 -0800

    changes to network_plotter.py from before submission

--- training/network_plotter.py
+++ training/network_plotter.py
@@ -3,11 +3,11 @@ import numpy as np
 import argparse
 from matplotlib import rc, rcParams
 rc('text',usetex=True)
-rcParams['axes.titlesize'] = 'x-large'
-rcParams['axes.labelsize'] = 'x-large'
-rcParams['xtick.labelsize'] = 'x-large'
-rcParams['ytick.labelsize'] = 'x-large'
-rcParams['legend.fontsize'] = 'large'
+#rcParams['axes.titlesize'] = 'x-large'
+#rcParams['axes.labelsize'] = 'x-large'
+#rcParams['xtick.labelsize'] = 'x-large'
+#rcParams['ytick.labelsize'] = 'x-large'
+#rcParams['legend.fontsize'] = 'large'
 """ --------------------------------------------------------------------------------------
    Command Line Arguments: Network Selection
 -----------------------------------------------------------------------------------------"""
@@ -32,11 +32,14 @@ def create_fig_file_name(folder_path, description_string, data_file_name):
 
 def plot_learning_curves(f1,f2,f3):
     f_loss,ax_loss = plt.subplots()
+    f_loss.set_tight_layout(True)
     f_accuracy,ax_accuracy = plt.subplots()
+    f_accuracy.set_tight_layout(True)
+    plt.tight_layout()
     nEpochs = 40
-    names = ['Logistic Regression', '2 Layer']
+    names = ['Logistic Regression', '2 Layer', '2 Layer Regu']
     color = ['C0', 'C1', 'C2']
-    for i, filename in enumerate([f1, f2]):
+    for i, filename in enumerate([f1, f2, f3]):
         data = np.loadtxt('./final_figures/' + filename + '.txt')
         epoch = data[:nEpochs,0]
         test_accuracy = data[:nEpochs,1]
@@ -49,6 +52,9 @@ def plot_learning_curves(f1,f2,f3):
         ax_loss.set_yscale('log')
         ax_loss.set_xlabel('Epoch \#')
         ax_loss.set_ylabel('Cross Entropy Loss')
+        #ax_loss.set_yticks(10**np.arange(-1,0.1))
+        ax_loss.set_yticks([0.2, 0.5, 1.0, 2.0])
+        #print(np.arange(-1,0,0.25))
         ax_loss.legend(loc = 'upper right')
 
         ax_accuracy.plot(epoch, training_accuracy, color[i]+':', label=names[i] + ': train')
@@ -56,6 +62,7 @@ def plot_learning_curves(f1,f2,f3):
         ax_accuracy.set_xlabel('Epoch \#')
         ax_accuracy.set_ylabel('Accuracy (\%)')
         ax_accuracy.legend(loc = 'lower right')
+    ax_loss.yaxis.set_major_formatter(plt.ScalarFormatter())
     figureName = create_fig_file_name('./final_figures/', 'LOSS_FIGURE', f1 + f2 + f3)
     f_loss.savefig(figureName, dpi = 600)
     figureName = create_fig_file_name('./final_figures/', 'ACCURACY_FIGURE', f1 + f2 + f3)
