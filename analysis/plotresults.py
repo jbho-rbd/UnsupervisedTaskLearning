37e3fd93ec1973468c470dd418bbe4c02f19d170
elena <elenagal@stanford.edu>
2020-10-31 20:32:19 -0700

    result plots scripts

--- analysis/plotresults.py
+++ analysis/plotresults.py
@@ -9,12 +9,46 @@
  
 Last update, Fall 2020
 ======================================================================================"""
-numTMatrixUpdates = 15 
-lastT = numTMatrixUpdates - 1
+import imageio
+import matplotlib.animation as ani
+import matplotlib.cm as cmx
+import matplotlib.colors as colors
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy as scipy
+import enum
+import sys
+import bisect
+import random
+import collections
+import os
+
+from matplotlib.patches import Ellipse
+from PIL import Image
+from sklearn import datasets
+from sklearn.cluster import KMeans
+
+from matplotlib.pyplot import figure, show
+from matplotlib.ticker import MaxNLocator
+
+from read_data import read_data1
+from plot_data import getlabels, plot_file , compute_success_rate
+
+# # latex formatting
+# rc('text',usetex=True)
+# rcParams['axes.titlesize'] = 'x-large'
+# rcParams['axes.labelsize'] = 'large'
+# rcParams['xtick.labelsize'] = 'x-large'
+# rcParams['ytick.labelsize'] = 'x-large'
+
+
+# numTMatrixUpdates = 1 
+# lastT = numTMatrixUpdates - 1
+# trans2plot = [0,lastT]
 print(">>>> Pato dibujando")
 
-run2plot = [2, 6, 18]
-trans2plot = [0,lastT]
+# run2plot = [2, 6, 18]
+run2plot = 20
 
 """
 CAP
@@ -43,17 +77,27 @@ CAP
 PIPE
 
 """     
-run2plot = [2, 12]
 trans2plot = 0
 
-for i in range(2): 
-    plot_file('../data/pipe/raw_pipe/run{0:d}'.format(run2plot[i]),
-        tlabelfile="results/run{0:d}_tlabels_T{1:d}".format(run2plot[i],trans2plot),
-        prlabelfile="results/run{0:d}_prmlabels_T{1:d}".format(run2plot[i],trans2plot)
-#       tlabelfileTruth='../data/pipe/raw_pipe/run{0:d}_tlabels'.format(run2plot[i]),
-#       prlabelfileTruth='../data/pipe/raw_pipe/run{0:d}_prmlabels'.format(run2plot[i])
-        )
-    plt.savefig("figures/labelled_run{0:d}_T{1:d}.png".format(run2plot[i],trans2plot),dpi=600)
-    plt.close()
+# for i in range(2): 
+#     plot_file('../data/pipe/raw_pipe/run{0:d}'.format(run2plot[i]),
+#         tlabelfile="results/run{0:d}_tlabels_T{1:d}".format(run2plot[i],trans2plot),
+#         prlabelfile="results/run{0:d}_prmlabels_T{1:d}".format(run2plot[i],trans2plot)
+# #       tlabelfileTruth='../data/pipe/raw_pipe/run{0:d}_tlabels'.format(run2plot[i]),
+# #       prlabelfileTruth='../data/pipe/raw_pipe/run{0:d}_prmlabels'.format(run2plot[i])
+#         )
+#     plt.savefig("figures/labelled_run{0:d}_T{1:d}.png".format(run2plot[i],trans2plot),dpi=600)
+#     plt.close()
+
+for i in range(1,20): 
+	print("run {0:d}".format(i))
+	plot_file('../data/pipe/raw_pipe/run{0:d}'.format(i), 
+    	tlabelfile="results/run{0:d}_tlabels_T{1:d}".format(i,trans2plot), 
+    	prlabelfile="results/run{0:d}_prmlabels_T{1:d}".format(i,trans2plot))
+	#       tlabelfileTruth='../data/pipe/raw_pipe/run{0:d}_tlabels'.format(i),
+	#       prlabelfileTruth='../data/pipe/raw_pipe/run{0:d}_prmlabels'.format(i)
+	plt.savefig("figures/labelled_run{0:d}_T{1:d}.png".format(i,trans2plot),dpi=600)
+	plt.title('Pipe Demonstration \#{0:d}'.format(i))
+	plt.close()
 
 print(">>>> Pato ha terminado de dibujar")
